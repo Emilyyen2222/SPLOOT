007c40d1d74285f95545a59737a4ca01cc8d7419
samuelboyy11 <samuelboyy11@gmail.com>
2025-01-09 10:52:51 +0800

    moved header into Banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="navHamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+
+        <div class="logo">
+            <a href="./">
+                <img src="@/assets/img/logo.svg" alt="Logo">
+            </a>
+        </div>
+        <nav>
+            <ul class="headerList">
+                <li class="headerItem"><a href="#">認識新毛友</a></li>
+                <li class="headerItem"><a href="#">訂閱寵物盒</a></li>
+                <li class="headerItem"><a href="#">尋找小幫手</a></li>
+                <li class="headerItem"><a href="#">毛孩日曆</a></li>
+                <li class="headerItem"><a href="#">友善設施</a></li>
+            </ul>
+        </nav>
+        <div class="loginBox">
+            <button class="login">登入</button>
+        </div>
+    </header>
+</template>
+  
+<script>
+  
+</script>
