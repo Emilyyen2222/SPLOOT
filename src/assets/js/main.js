c7692d88e4cc404385ccf96d7796da4eaecffe64
samuelboyy11 <samuelboyy11@gmail.com>
2025-01-10 13:36:56 +0800

    vue-router first upload

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "@/router/index.js";
+import Main from "@/components/Main.vue";
+
+const pinia = createPinia();
+
+createApp(Main).use(router).use(pinia).mount('#app');
