487cb1f3f0b876a2983e32073b28fd5c2ae6f25a
samuelboyy11 <samuelboyy11@gmail.com>
2025-01-13 17:24:29 +0800

    created new vue component inputText

---
+++ src/components/InputText.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="inputBox" :class="{'-error': hasError}">
+
+    <input 
+    :type="inputType"
+    :placeholder="placeHolder"
+    :value="modelValue"
+    @input="updateValue">
+  
+    <p v-if="hasError" class="errorMsg">{{ errorMsg }}</p>
+  </div>
+</template>
+
+<script setup>
+  import { ref, defineProps, defineEmits } from 'vue';
+
+  const props = defineProps({
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    placeHolder: {
+      type: String,
+      required: true,
+    },
+    errorMsg: {
+      type: String,
+      required: true,
+    },
+    hasError: {
+      type: Boolean,
+      default: false
+    }
+  });
+
+  const emit = defineEmits(['update:modelValue']);
+
+  const updateValue = (e) => {
+    emit('update:modelValue', e.target.value);
+  };
+</script>
