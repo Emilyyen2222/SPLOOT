1a3dc9a82ca01ed567b355a9371e9af2b61423ab
samuelboyy11 <samuelboyy11@gmail.com>
2025-02-01 15:28:50 +0800

    added progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="progressBar">
+    <div v-for="index in total" :key="index" 
+    class="progressSegment"
+    :class="['progressSegment', { filled: index <= current }]"></div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    total: {
+      type: Number,
+      required: true
+    },
+    current: {
+      type: Number,
+      required: true
+    }
+  });
+</script>
