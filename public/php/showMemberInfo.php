e6cc17a6612afd4c9356ec808d8fb1cf3e6c636b
Hao-code184 <yf960021@gmail.com>
2025-02-16 20:11:38 +0800

    update: showMemberInfo.php: post

---
+++ public/php/showMemberInfo.php
@@ -0,0 +1,57 @@
+<?php
+include 'PdoConnection.php';
+
+header("Content-Type:application/json");
+
+$postData = json_decode(file_get_contents("php://input"), true);
+$response = [];
+
+
+$user_id = '3';
+$sql_select = "
+  select
+    email,
+    password,
+    first_name as firstName,
+    last_name as lastName,
+    nickname,
+    gender,
+    birth_date as birthDate,
+    line_id as lineId,
+    phone,
+    address_city as addressCity,
+    address_district as addressDistrict,
+    address_street as addressStreet
+  from USER
+  WHERE user_id = :user_id
+";
+
+$stmt_select = $pdo->prepare($sql_select);
+$stmt_select->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+$stmt_select->execute();
+$user = $stmt_select->fetch(PDO::FETCH_ASSOC);
+
+if ($user){
+  $response = [
+    'status' => 'success',
+    'email' => $user['email'],
+    'firstName' => $user['firstName'],
+    'lastName' => $user['lastName'],
+    'nickname' => $user['nickname'],
+    'gender' => $user['gender'],
+    'birthDate' => $user['birthDate'],
+    'lineId' => $user['lineId'],
+    'phone' => $user['phone'],
+    'addressCity' => $user['addressCity'],
+    'addressDistrict' => $user['addressDistrict'],
+    'addressStreet' => $user['addressStreet']
+  ];
+}else{
+  $response = [
+    'status' => 'error',
+    'message' => '無使用者'
+  ];
+}
+
+echo json_encode($response);
+?>
